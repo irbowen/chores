79075aa3d290a6b5f3b1de13579f02742d338139
Isaac Bowen <irbowen@umich.edu>
2016-12-08 12:58:05 -0500

    Cleanup

--- automation/mail_chores/mail_chores.sh
+++ automation/mail_chores/mail_chores.sh
@@ -1,15 +1,21 @@
 # Just to avoid error
-touch mailmege_database.csv
-rm mailmege_database.csv
+touch mailmerge_database.csv
+rm mailmerge_database.csv
 
 # Copy over the header to the database
 cp mailmerge_starting_database.csv mailmerge_database.csv
 
 # Assign chores and concat to end of file
-python3 assign_chores.py | sort >> mailmerge_database.csv
-grep . mailmege_database.csv | sort > mailmege_database.csv
-# sed -i '/^$/d' mailmerge_database.csv
+echo 'Runing some python3'
+python3 assign_chores.py | grep . | sort >> mailmerge_database.csv
+cat mailmerge_database.csv
+echo "Is this okay?"
+read input
+if [ $input == "yes" ]; then
+  echo "Doing some things"
+  mailmerge --no-dry-run --no-limit
+else
+  echo "Nope"
+fi
 
-# Mail the chores out
-mailmerge --dry-run --no-limit
 
